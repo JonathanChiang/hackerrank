689dbe9ca0899a24f4421fbfd7e270cdc5faac49
Amir Kurtovic <amirkurtovic@gmail.com>
2014-07-22 22:12:20 -0500

    Adding Shortest FizzBuzz

---
+++ shortestFizzBuzz.py
@@ -0,0 +1,18 @@
+"""
+Shortest FizzBuzz
+
+Write a program that prints (to STDOUT) the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number and
+for the multiples of five print "Buzz". For numbers which are
+multiples of both three and five print "FizzBuzz".
+
+The goal is to write the shortest code possible.
+
+Scoring: Your score is (200 - number of characters in your source
+code)/10
+"""
+
+a = [x for x in range(1, 101)]       
+b = ["FizzBuzz" if y%15==0 else "Buzz" if y%5==0 else "Fizz" if y%3==0 else y for y in a]
+for z in b:
+    print z
