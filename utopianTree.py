3266222b4a974f2c470c9f442ef9b7b74e42e532
Amir Kurtovic <amirkurtovic@gmail.com>
2014-07-25 08:23:50 -0500

    Update utopianTree.py

--- utopianTree.py
+++ utopianTree.py
@@ -1,6 +1,9 @@
 """
-The Utopian tree goes through 2 cycles of growth every year. The first growth cycle of the tree occurs during the monsoon, when it doubles in height. The second growth cycle of the tree occurs during the summer, when its height increases by 1 meter. 
-Now, a new Utopian tree sapling is planted at the onset of the monsoon. Its height is 1 meter. Can you find the height of the tree after N growth cycles?
+The Utopian tree goes through 2 cycles of growth every year. The first growth cycle of the tree
+occurs during the monsoon, when it doubles in height. The second growth cycle of the tree occurs
+during the summer, when its height increases by 1 meter. 
+Now, a new Utopian tree sapling is planted at the onset of the monsoon. Its height is 1 meter. 
+Can you find the height of the tree after N growth cycles?
 
 Input Format 
 The first line contains an integer, T, the number of test cases. 
